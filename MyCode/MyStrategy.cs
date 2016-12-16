73f8cde57a36cf452e91de46318eef21d3b84850
Andrey Morozov <andre15@inbox.ru>
2016-12-16 23:58:35 +0400

    проверка на башни

--- MyCode/MyStrategy.cs
+++ MyCode/MyStrategy.cs
@@ -3929,6 +3929,7 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk
                 if (target.Faction == _self.Faction) continue;
                 if (!IsOkDistanceToShoot(_self, target, 0d)) continue;
                 if (IsBlockingTree(_self, target, _game.MagicMissileRadius)) continue;
+                if(!IsStrongOnLine(target, _line)) continue;
 
                 double distance = _self.GetDistanceTo(target);
 
@@ -4114,6 +4115,7 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk
                 if (target.Faction == _self.Faction) continue;
                 if (!IsOkDistanceToShoot(_self, target, 0d)) continue;
                 if (IsBlockingTree(_self, target, _game.MagicMissileRadius)) continue;
+                if (!IsStrongOnLine(target, _line)) continue;
 
                 double distance = _self.GetDistanceTo(target);
 
