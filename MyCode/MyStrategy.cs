68f36fe669c5e13baab51d93f42fe1501b96f2db
Andrey Morozov <andre15@inbox.ru>
2016-12-17 00:12:14 +0400

    баг с бонусами

--- MyCode/MyStrategy.cs
+++ MyCode/MyStrategy.cs
@@ -1368,7 +1368,8 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk
             //если игра 1 на 1
             if (_isOneOneOne)
             {
-                //if (GetMyLineType(_line) == LineType.Defensive || _myWizards[_line].Count - _anemyWizards[_line].Count >= 1) return goBonusResult;
+                
+                if (GetMyLineType(_line) == LineType.Defensive || _myWizards[_line].Count - _anemyWizards[_line].Count >= 1) return goBonusResult;
                 var ordered0Wizards =
                     _world.Wizards.Where(x => x.Faction == _self.Faction).OrderBy(x => _bonusPoints[0].getDistanceTo(x));
 
@@ -1391,10 +1392,12 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk
                                 break;
                             }
                         }
-                        if (wizardLaneType != null && GetMyLineType(wizardLaneType.Value) != LineType.Defensive &&
-                            _myWizards[wizardLaneType.Value].Count - _anemyWizards[wizardLaneType.Value].Count <= 1)
+                       
+                        if (wizardLaneType != null && GetMyLineType(wizardLaneType.Value) != LineType.Defensive)
                         {
-                            break;
+                            var myWizardsCount = _myWizards[wizardLaneType.Value].Count;
+                            if (_line == wizardLaneType.Value) myWizardsCount++;
+                            if (myWizardsCount - _anemyWizards[wizardLaneType.Value].Count <= 1) break;
                         }
                     }
                 }
